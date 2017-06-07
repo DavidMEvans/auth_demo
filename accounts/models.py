d260be810d4d81320b3faa6457feddbd206e31e6
David Evans <devans6915@aol.com>
2017-06-07 10:51:37 +0100

    Add files via upload

---
+++ accounts/models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+# Create your models here.
+
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.db import models
+from django.utils import timezone
